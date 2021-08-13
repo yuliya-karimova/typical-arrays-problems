f5e951448cbd5b92f68e6907cdd9083b2121b369
yuliya-karimova <y.o.karimova@gmail.com>
2021-08-13 14:41:41 +0300

    feat: add tast solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return array && array.length > 0 ? Math.min.apply(Math, array) : 0; 
 }
 
 exports.max = function max (array) {
-  return 0;
+    return array && array.length > 0 ? Math.max.apply(Math, array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return array && array.length > 0 ? array.reduce((a, b) => a + b, 0) / array.length : 0;
 }
